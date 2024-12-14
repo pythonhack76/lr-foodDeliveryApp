a9ee058acb704d02c49d0f9c63133cf30b3943a1
pythonthehack <lrulvoni@gmail.com>
2024-12-14 09:45:37 +0100

    add asset logo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image
+        source={require('./assets/logo-lr-food-delivery.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Food Delivery App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  }
 });
